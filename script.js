3e5790dad1cde8ce5b83ac64da8094c89dae8ee2
Nicole Shek <Nicole@SM-Macbook-0329.local>
2022-02-10 11:08:44 +0000

    added display grid to images

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ setTimeout(() => {
   document.querySelector(".loadingScreen").style.display = "none";
   document.querySelector("main").style.display = "block";
 }, 4300);
+
+// picture grid
+let options = {};
+
+let pictureGrid = document.querySelector(".pictureGrid");
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+  });
+});
+
+observer.observe(pictureGrid);
