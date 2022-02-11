e3e9409bae5b7dfb895cfc0a74cbca552441a86d
SCREENMEDIA\Nicole.Shek <nicole.shek@screenmedia.co.uk>
2022-02-11 12:49:54 +0000

    added text fade in to video

--- scrollEffect.js
+++ scrollEffect.js
@@ -1,11 +1,12 @@
 window.addEventListener("DOMContentLoaded", scrollEffect);
 
+// local variables
+const scrollEffectWrapper = document.querySelector(".scrollEffectWrapper");
+const variables = document.querySelector(":root");
+const video = document.querySelector(".middleVideo video");
+const videoText = document.querySelector(".middleVideo .text");
+
 function scrollEffect() {
-  // local variables
-  const scrollEffectWrapper = document.querySelector(".scrollEffectWrapper");
-
-  const variables = document.querySelector(":root");
-
   // observer
   const options = {
     threshold: 1,
@@ -22,18 +23,29 @@ function scrollEffect() {
   observer.observe(scrollEffectWrapper);
 
   function moveSidePanes() {
+    console.log("SCROLL TOP SCROLL EFFECT", video.getBoundingClientRect().top);
+    // console.log("SCROLL TOP WINDOW", window.scroll);
+
+    // show text when video reaches certain scroll point
+    showVideoText();
+
     let leftSideTranslate = window.scrollY * -1;
     let rightSideTranslate = window.scrollY;
 
     setVariable("--left-pane-translateX", leftSideTranslate, "px");
     setVariable("--right-pane-translateX", rightSideTranslate, "px");
   }
+}
 
-  // Helper functions
-  function setVariable(variableName, variableValue, variableUnit) {
-    variables.style.setProperty(
-      variableName,
-      `${variableValue}${variableUnit}`
-    );
+// Helper functions
+function setVariable(variableName, variableValue, variableUnit) {
+  variables.style.setProperty(variableName, `${variableValue}${variableUnit}`);
+}
+
+function showVideoText() {
+  if (video.getBoundingClientRect().top <= 50) {
+    videoText.style.opacity = 1;
+  } else {
+    videoText.style.opacity = 0;
   }
 }
